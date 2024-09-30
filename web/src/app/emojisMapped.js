a430a5f724511aa02876369da648af91d2c58d98
SpiralP <spiral.palmer@gmail.com>
2024-09-30 11:50:31 -0700

    rm web

--- web/src/app/emojisMapped.js
+++
@@ -1,4 +0,0 @@
-import { rawEmojis } from "./emojis";
-
-// Format emojis (see emoji.js)
-export default Object.fromEntries(rawEmojis.flatMap((emoji) => emoji.aliases.map((alias) => [alias, emoji.emoji])));
